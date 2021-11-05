e80d6b665af1b678cd1dc6578be0856f9e11bd26
Nuanjan <nuanjan.schluntz@gmail.com>
2021-11-04 23:25:28 -0400

    Add hom route and all Author sort by name

--- authors_client/src/components/Error.jsx
+++ authors_client/src/components/Error.jsx
@@ -4,7 +4,8 @@ const Error = () => {
     return (
         <div>
             <h1>sorry, but we could not find the author you are looking for. Would you like to add this author to our database?</h1>
-            <a href={"/new"}>Add an Author</a>
+            <a href={"/new"}>Add an Author</a><span> | </span>
+            <a href={"/"}>Home</a>
         </div>
     );
 };
